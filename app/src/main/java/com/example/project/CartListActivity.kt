d3ec97d9820494c990d69e4fa248ed5cfb3c23cd
gerardoJoseVilledaErazo <gerardovilleda@gmail.com>
2022-12-02 09:13:57 -0600

    10o Commit: Se arreglaron unos errores de confusion que se tenian en el activity_cart_list.xml confusion de deliveryTxt y taxTxt, se creo un emptyTxt y se modifico el CartListActivity.kt

--- app/src/main/java/com/example/project/CartListActivity.kt
+++ app/src/main/java/com/example/project/CartListActivity.kt
@@ -1,11 +1,56 @@
 package com.example.project
 
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.ScrollView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.project.databinding.ActivityCartListBinding
+import com.example.project.databinding.ActivityShowDetailBinding
+import com.example.project.helper.ManagementCart
 
 class CartListActivity : AppCompatActivity() {
+
+    //    private lateinit var adapter: RecyclerView.Adapter
+    //    private lateinit var adapter: CartListAdapter
+    private lateinit var managementCart: ManagementCart
+    private var linearLayoutManager: LinearLayoutManager =
+        LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+    private var tax: Double = 0.0
+    private lateinit var scrollView: ScrollView
+
+    // Variable para manejar el viewBinding
+    private lateinit var binding: ActivityCartListBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cart_list)
+//        setContentView(R.layout.activity_cart_list)
+        //Configuracion de ViewBinding
+        binding = ActivityCartListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        // Se agrega la flecha para salir de la actividad
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        // Se configura el nombre de la actividad
+        title = "Cart List"
+
+        managementCart = ManagementCart(this)
+    }
+
+    private fun initList() {
+        binding.recyclerViewList.layoutManager = linearLayoutManager
+    }
+
+    // Se configura la flecha para salir de la actividad
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                // Finalizar la actividad
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
